3aa24434fe77950eea9b10b75c1f4268ba4d876d
shamoji8 <hn.11235.natu@gmail.com>
2025-08-25 22:12:31 +0900

    Site updated: 2025-08-25 22:12:31

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1756127209888";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1756127550828";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
